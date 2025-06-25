78409bbefcd2e0ca6fdaa56c5d4bb42464d2d1e8
haedonk <haka9670@gmail.com>
2025-06-24 23:33:11 -0400

    feat: enable scheduling in RecipeAppApplication for improved task management

--- src/main/java/com/haekitchenapp/recipeapp/RecipeAppApplication.java
+++ src/main/java/com/haekitchenapp/recipeapp/RecipeAppApplication.java
@@ -1,12 +1,13 @@
 package com.haekitchenapp.recipeapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity
+@EnableScheduling
 public class RecipeAppApplication {
 
 	public static void main(String[] args) {
